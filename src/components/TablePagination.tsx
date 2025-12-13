2184f861c4333f0b3dd140d14080634780f8e441
NaingWinAung <naingwinaung1710@gmail.com>
2025-12-13 22:21:29 +0700

    small change

--- src/components/TablePagination.tsx
+++ src/components/TablePagination.tsx
@@ -34,19 +34,16 @@ const TablePagination = ({
     const pages: (number | "ellipsis")[] = [];
 
     if (totalPages <= 7) {
-      // Show all pages if total is 7 or less
       for (let i = 1; i <= totalPages; i++) {
         pages.push(i);
       }
     } else {
-      // Always show first page
       pages.push(1);
 
       if (currentPage > 3) {
         pages.push("ellipsis");
       }
 
-      // Show pages around current page
       const start = Math.max(2, currentPage - 1);
       const end = Math.min(totalPages - 1, currentPage + 1);
 
@@ -58,7 +55,6 @@ const TablePagination = ({
         pages.push("ellipsis");
       }
 
-      // Always show last page
       pages.push(totalPages);
     }
 
