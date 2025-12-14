ac7d71f9ba297e0139cb5abb691afd17477bfb18
NaingWinAung <naingwinaung1710@gmail.com>
2025-12-14 15:01:45 +0700

    product form add multiple select

--- src/pages/products/partials/ProductForm.tsx
+++ src/pages/products/partials/ProductForm.tsx
@@ -3,9 +3,18 @@ import FormError from "@/components/global/FormError";
 import FormInput from "@/components/global/FormInput";
 import FormLabel from "@/components/global/FormLabel";
 import { useForm, type SubmitHandler } from "react-hook-form";
+import {
+  MultiSelect,
+  MultiSelectContent,
+  MultiSelectGroup,
+  MultiSelectItem,
+  MultiSelectTrigger,
+  MultiSelectValue,
+} from "@/components/ui/multi-select";
 
 type FormFields = {
   name: string;
+  piers: string[];
 };
 
 const ProductForm = ({ isCreate }: { isCreate: boolean }) => {
@@ -35,17 +44,40 @@ const ProductForm = ({ isCreate }: { isCreate: boolean }) => {
         <div className="text-red-500 text-sm mt-1.5">{errors.root.message}</div>
       )}
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-6">
-          <FormLabel htmlFor="name">Name</FormLabel>
-          <FormInput
-            id="name"
-            placeholder="Enter name"
-            {...register("name", {
-              required: "Name is required",
-            })}
-          />
-          {errors.name && <FormError message={errors.name.message} />}
+        <div className="flex items-center gap-4">
+          <div className="w-1/2 mb-6">
+            <FormLabel htmlFor="name">Name</FormLabel>
+            <FormInput
+              id="name"
+              placeholder="Enter name"
+              {...register("name", {
+                required: "Name is required",
+              })}
+            />
+            {errors.name && <FormError message={errors.name.message} />}
+          </div>
+
+          <div className="w-1/2 mb-6">
+            <FormLabel htmlFor="piers">Piers</FormLabel>
+            <MultiSelect>
+              <MultiSelectTrigger className="w-full">
+                <MultiSelectValue placeholder="Select frameworks..." />
+              </MultiSelectTrigger>
+              <MultiSelectContent>
+                <MultiSelectGroup>
+                  <MultiSelectItem value="next.js">Next.js</MultiSelectItem>
+                  <MultiSelectItem value="sveltekit">SvelteKit</MultiSelectItem>
+                  <MultiSelectItem value="astro">Astro</MultiSelectItem>
+                  <MultiSelectItem value="vue">Vue.js</MultiSelectItem>
+                  <MultiSelectItem value="react">React</MultiSelectItem>
+                  <MultiSelectItem value="angular">Angular</MultiSelectItem>
+                </MultiSelectGroup>
+              </MultiSelectContent>
+            </MultiSelect>
+            {errors.piers && <FormError message={errors.piers.message} />}
+          </div>
         </div>
+
         <div className="flex justify-end">
           <FormButton disabled={isSubmitting}>
             {isCreate ? "Save" : "Update"}
