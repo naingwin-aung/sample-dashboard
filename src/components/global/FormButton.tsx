f1eb607a76ad7c3a788fd422ee2fc9e940684094
NaingWinAung <naingwinaung1710@gmail.com>
2025-12-15 22:08:11 +0700

    local form submiting

--- src/components/global/FormButton.tsx
+++ src/components/global/FormButton.tsx
@@ -1,8 +1,9 @@
-const FormButton = ({ type, disabled, children }: { type?: "button" | "submit" | "reset"; disabled?: boolean; children: React.ReactNode }) => {
+const FormButton = ({ type, disabled, onClick, children }: { type?: "button" | "submit" | "reset"; disabled?: boolean; onClick?: React.MouseEventHandler<HTMLButtonElement>; children: React.ReactNode }) => {
   return (
     <button
       type={type || "submit"}
       disabled={disabled}
+      onClick={onClick}
       className="text-sm bg-gray-800 text-white px-5 py-2.5 rounded-md cursor-pointer hover:bg-gray-900 transition disabled:opacity-50 disabled:cursor-default"
     >
       {children}
--- src/pages/products/partials/BoatDialog.tsx
+++ src/pages/products/partials/BoatDialog.tsx
@@ -102,117 +102,119 @@ const BoatDialog = ({
   return (
     <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
       <DialogTrigger asChild>
-        <Plus size={19} className="cursor-pointer ms-3.5 text-gray-600 hover:text-gray-900" />
+        <Plus
+          size={19}
+          className="cursor-pointer ms-3.5 text-gray-600 hover:text-gray-900"
+        />
       </DialogTrigger>
       <DialogContent className="sm:max-w-5xl">
         <DialogHeader>
           <DialogTitle className="mb-5">
             {isEditing ? "Edit Boat" : "Add Boat"}
           </DialogTitle>
-          <form onSubmit={handleSubmit(handleLocalSubmit)}>
-            <Tabs defaultValue="schedule">
-              <TabsList>
-                <TabsTrigger value="schedule">Schedule</TabsTrigger>
-              </TabsList>
-              <TabsContent value="schedule">
-                <div className="flex items-center gap-4 mt-3 mb-7">
-                  <div className="w-1/3">
-                    <FormLabel htmlFor="boat">Boat</FormLabel>
-                    <FormInput
-                      id="boat"
-                      placeholder="Select boat"
-                      {...register("boat_id")}
-                    />
-                  </div>
-                  <div className="w-1/3">
-                    <FormLabel htmlFor="start_date">
-                      Available start date
-                    </FormLabel>
-                    <FormInput
-                      id="start_date"
-                      placeholder="Select start date"
-                      {...register("start_date")}
-                    />
-                  </div>
-                  <div className="w-1/3">
-                    <FormLabel htmlFor="end_date">Available end date</FormLabel>
-                    <FormInput
-                      id="end_date"
-                      placeholder="Select end date"
-                      {...register("end_date")}
-                    />
-                  </div>
+          <Tabs defaultValue="schedule">
+            <TabsList>
+              <TabsTrigger value="schedule">Schedule</TabsTrigger>
+            </TabsList>
+            <TabsContent value="schedule">
+              <div className="flex items-center gap-4 mt-3 mb-7">
+                <div className="w-1/3">
+                  <FormLabel htmlFor="boat">Boat</FormLabel>
+                  <FormInput
+                    id="boat"
+                    placeholder="Select boat"
+                    {...register("boat_id")}
+                  />
                 </div>
+                <div className="w-1/3">
+                  <FormLabel htmlFor="start_date">
+                    Available start date
+                  </FormLabel>
+                  <FormInput
+                    id="start_date"
+                    placeholder="Select start date"
+                    {...register("start_date")}
+                  />
+                </div>
+                <div className="w-1/3">
+                  <FormLabel htmlFor="end_date">Available end date</FormLabel>
+                  <FormInput
+                    id="end_date"
+                    placeholder="Select end date"
+                    {...register("end_date")}
+                  />
+                </div>
+              </div>
 
-                <h3 className="font-medium">Schedule Times</h3>
-                <div className="overflow-x-auto border border-gray-200 rounded-lg shadow-xs mb-5 mt-3">
-                  <table className="min-w-full divide-y divide-gray-200 shadow-lg rounded-lg">
-                    <thead>
-                      <tr>
-                        <th className="w-0.5">
-                          <Plus
-                            size={19}
-                            className="ms-2.5 text-gray-600 cursor-pointer"
-                            onClick={() =>
-                              appendSchedule(getNewScheduleTemplate())
-                            }
+              <h3 className="font-medium">Schedule Times</h3>
+              <div className="overflow-x-auto border border-gray-200 rounded-lg shadow-xs mb-5 mt-3">
+                <table className="min-w-full divide-y divide-gray-200 shadow-lg rounded-lg">
+                  <thead>
+                    <tr>
+                      <th className="w-0.5">
+                        <Plus
+                          size={19}
+                          className="ms-2.5 text-gray-600 cursor-pointer"
+                          onClick={() =>
+                            appendSchedule(getNewScheduleTemplate())
+                          }
+                        />
+                      </th>
+                      <th className="px-6 py-3 text-left text-sm font-medium tracking-wide text-gray-600">
+                        Start time
+                      </th>
+                      <th className="px-6 py-3 text-left text-sm font-medium tracking-wide text-gray-600">
+                        End time
+                      </th>
+                      <th className="px-6 py-3 text-left text-sm font-medium tracking-wide text-gray-600"></th>
+                    </tr>
+                  </thead>
+
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {scheduleFields.map((schedule, index) => (
+                      <tr key={schedule.id}>
+                        <td className="w-0.5"></td>
+                        <td className="px-6 py-3 whitespace-nowrap text-sm">
+                          <FormInput
+                            id="start_time"
+                            placeholder="Enter start time"
+                            {...register(`schedule_times.${index}.start_time`)}
                           />
-                        </th>
-                        <th className="px-6 py-3 text-left text-sm font-medium tracking-wide text-gray-600">
-                          Start time
-                        </th>
-                        <th className="px-6 py-3 text-left text-sm font-medium tracking-wide text-gray-600">
-                          End time
-                        </th>
-                        <th className="px-6 py-3 text-left text-sm font-medium tracking-wide text-gray-600"></th>
+                        </td>
+                        <td className="px-6 py-3 whitespace-nowrap text-sm">
+                          <FormInput
+                            id="end_time"
+                            placeholder="Enter end time"
+                            {...register(`schedule_times.${index}.end_time`)}
+                          />
+                        </td>
+                        <td className="px-6 py-3 text-end">
+                          {scheduleFields.length > 1 && (
+                            <span>
+                              <Trash2
+                                size={18}
+                                strokeWidth={2.1}
+                                className="inline text-red-600 cursor-pointer"
+                                onClick={() => removeSchedule(index)}
+                              />
+                            </span>
+                          )}
+                        </td>
                       </tr>
-                    </thead>
-
-                    <tbody className="bg-white divide-y divide-gray-200">
-                      {scheduleFields.map((schedule, index) => (
-                        <tr key={schedule.id}>
-                          <td className="w-0.5"></td>
-                          <td className="px-6 py-3 whitespace-nowrap text-sm">
-                            <FormInput
-                              id="start_time"
-                              placeholder="Enter start time"
-                              {...register(
-                                `schedule_times.${index}.start_time`
-                              )}
-                            />
-                          </td>
-                          <td className="px-6 py-3 whitespace-nowrap text-sm">
-                            <FormInput
-                              id="end_time"
-                              placeholder="Enter end time"
-                              {...register(`schedule_times.${index}.end_time`)}
-                            />
-                          </td>
-                          <td className="px-6 py-3 text-end">
-                            {scheduleFields.length > 1 && (
-                              <span>
-                                <Trash2
-                                  size={18}
-                                  strokeWidth={2.1}
-                                  className="inline text-red-600 cursor-pointer"
-                                  onClick={() => removeSchedule(index)}
-                                />
-                              </span>
-                            )}
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-                <div className="flex justify-end items-center">
-                  <FormButton type="submit" disabled={isSubmitting}>
-                    {isEditing ? "Update" : "Save"}
-                  </FormButton>
-                </div>
-              </TabsContent>
-            </Tabs>
-          </form>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <div className="flex justify-end items-center">
+                <FormButton
+                  onClick={handleSubmit(handleLocalSubmit)}
+                  disabled={isSubmitting}
+                >
+                  {isEditing ? "Update" : "Save"}
+                </FormButton>
+              </div>
+            </TabsContent>
+          </Tabs>
         </DialogHeader>
       </DialogContent>
     </Dialog>
