4f850789840a66f96718faa86877c664ef76094b
NaingWinAung <naingwinaung1710@gmail.com>
2025-12-16 23:09:47 +0700

    existing image gallery

--- src/components/global/FormLabel.tsx
+++ src/components/global/FormLabel.tsx
@@ -1,6 +1,6 @@
-const FormLabel = ({ htmlFor, children }: { htmlFor: string; children: React.ReactNode }) => {
+const FormLabel = ({ htmlFor, children, className }: { htmlFor?: string; children: React.ReactNode; className?: string }) => {
   return (
-    <label htmlFor={htmlFor} className="block mb-2 text-sm text-gray-700">
+    <label htmlFor={htmlFor} className={`block mb-2 text-sm text-gray-700 ${className ?? ""}`}>
       {children}
     </label>
   );
